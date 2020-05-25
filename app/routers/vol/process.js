d4d27de53170fe4a4fac0f2fa6eb246687f7db41
BHY <inomp3042@naver.com>
2020-05-25 16:58:47 +0900

    기존 코드 복구

--- app/routers/vol/process.js
+++ app/routers/vol/process.js
@@ -105,9 +105,11 @@ router.route('/save').post(async function(req,res){
     var vol_date = req.body.vol_date;
     var vol_time = req.body.vol_time;
 
+    // invoke_chain(student_name, vol_name,vol_date,vol_time);
+
     try {
         const fovvol = await FovVol('admin');
-        await fovvol.invokeChain(vol_time, vol_date, vol_name, student_name);
+        await fovvol.invokeChain(vol_time, vol_date, '숭실봉사센터', vol_name);
     } catch (err) {
         console.log(err);
     }
@@ -115,4 +117,49 @@ router.route('/save').post(async function(req,res){
     }
 });
 
+
+async function invoke_chain(student_name, vol_name, vol_date, vol_time) {
+    console.log('==========INVOKE FUNC START==========');
+try {
+        // load the network configuration
+        const ccpPath = path.resolve(config.path.FABRIC_NETWORK_PATH, 'organizations', 'peerOrganizations', 'vol1.example.com', 'connection-vol1.json');
+        let ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new file system based wallet for managing identities.
+        const volentPath = path.join(process.cwd(), 'volent');
+
+//      await volent = new FileSystemWallet(volentPath);
+        const volent = await Wallets.newFileSystemWallet(volentPath);
+        console.log(`Volent path: ${volentPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const identity = await volent.get('admin');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the volent');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet: volent, identity: 'admin', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fov');//networt 이름 fov
+
+        // Submit the specified transaction.
+        await contract.submitTransaction('createVolent', 'VOLENT', vol_time, vol_date, vol_name, student_name);
+        console.log('Transaction has been submitted');
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
 module.exports = router;
